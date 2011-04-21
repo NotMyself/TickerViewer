50e743a5c642656d533db69f5456af76a83e28c9
Bobby Johnson <bobby.johnson@gmail.com>
2011-04-21 11:00:19 -0700

    reverting back to silly NBuilder version

--- src/TickerPerformanceViewer.Web/Services/PriceLookupService.cs
+++ src/TickerPerformanceViewer.Web/Services/PriceLookupService.cs
@@ -16,7 +16,7 @@ namespace TickerPerformanceViewer.Web.Services
     {
         private readonly string yahooFinanceQuery = "http://ichart.finance.yahoo.com/table.csv?s={0}";
         private WebClient webClient;
-        FileHelperEngine<DailyPrice> reader;
+        private FileHelperEngine<DailyPrice> reader;
 
         public PriceLookupService()
         {
@@ -25,9 +25,23 @@ namespace TickerPerformanceViewer.Web.Services
         }
         public IEnumerable<DailyPrice> GetFor(string symbol)
         {
-            var uri = string.Format(yahooFinanceQuery, symbol);
-            var rawData = webClient.DownloadString(uri);
-            return reader.ReadString(rawData);
+            //TODO: look up via http://ichart.finance.yahoo.com/table.csv?s=MSFT
+            var random = new RandomGenerator(42);
+            var price = 100f;
+            return Builder<DailyPrice>.CreateListOfSize(20)
+                .WhereAll().Have(x => x.Open = price - random.Int())
+                .And(x => x.Close = price - random.Int())
+                .And(x => x.High = price - random.Int())
+                .And(x => x.Low = price - random.Int())
+                .Build();
         }
+
+        //public IEnumerable<DailyPrice> GetFor(string symbol)
+        //{
+        //    var uri = string.Format(yahooFinanceQuery, symbol);
+        //    var rawData = webClient.DownloadString(uri);
+        //    return reader.ReadString(rawData);
+        //}
+
     }
 }
